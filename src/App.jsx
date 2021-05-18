e6a72e1fe1d8cf64daf8d8dddf714bb46143244c
Jordan Bryan <jbryan382@gmail.com>
2021-05-18 19:30:12 -0400

    Example completed for nightshift students

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,34 @@ import { useEffect, useState } from 'react'
 
 export function App() {
   // Storing our Data in a useState
+  const [items, setItems] = useState([])
   const [ACCESS_TOKEN, setACCESS_TOKEN] = useState('jordan');
-  const [Item, setItem] = useState([]);
 
   // Custom async function to GET a list from one-list-api.herokuapp.com
   async function handleGetFromList() {
-    // Fetch to url
+    // Fetch to the url
     const response = await fetch(`https://one-list-api.herokuapp.com/items?access_token=${ACCESS_TOKEN}`)
-    // Converting response to JSON
+    // Convert the response to JSON
     const data = await response.json()
-    // Setting out Item useState to our response data
-    setItem(data)
+    // Set our Item(s) to our response data
+    setItems(data)
   }
 
   // Setting a useEffect to occur upon page load, and when the ACCESS_TOKEN is changed
   useEffect(() => {
-    console.log('SEE IT HAPPEN!');
+    console.log("SOMETHING IS CHANGING")
     handleGetFromList()
     // vv Checking for ACCESS_TOKEN change vv
-  }, [ACCESS_TOKEN]);
+  }, [ACCESS_TOKEN])
 
-  return <div>
+  return <>
     <h1>One List:</h1>
     {/* An input to alter the ACCESS_TOKEN */}
     <input type="text" value={ACCESS_TOKEN} onChange={(event) => setACCESS_TOKEN(event.target.value)} />
     <ul>
-      {/* Mapping out Item (our list of items) */}
-      {Item.map((listItem, index) => {
-        return (<li key={index}>{listItem.text}</li>)
-      })}
+      {items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
     </ul>
-  </div>
+  </>
 }
